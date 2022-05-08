e4cf11da5009358eb1c5c321e898aeaf4f0d18fe
Zlakowski <zlakos@o2.pl>
2022-05-08 20:45:29 +0200

    add donation model

--- portfoliolab_app/models.py
+++ portfoliolab_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,9 +7,6 @@ class Category(models.Model):
 
 
 class Institution(models.Model):
-    name = models.CharField(max_length=256, verbose_name='Nazwa instytucji')
-    decription = models.TextField()
-
     FUNDACJA = 'fundacja'
     ORGANIZACJA_POZARZADOWA = 'organizacja pozarządowa'
     ZBIORKA_LOKALNA = 'zbiórka lokalna'
@@ -18,5 +16,22 @@ class Institution(models.Model):
         (ORGANIZACJA_POZARZADOWA, 'organizacja pozarządowa'),
         (ZBIORKA_LOKALNA, 'zbiórka lokalna')
     )
+
+    name = models.CharField(max_length=256, verbose_name='Nazwa instytucji')
+    decription = models.TextField()
     type = models.CharField(max_length=32, choices=TYPES, default=FUNDACJA)
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=32)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField(auto_now=False)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
