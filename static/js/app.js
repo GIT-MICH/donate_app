405b80aac56bd8ff3c31c83633ee269ddde8aea5
Zlakowski <zlakos@o2.pl>
2022-05-18 22:44:31 +0200

    modify app and form files

--- static/js/app.js
+++ static/js/app.js
@@ -1,320 +1,344 @@
-document.addEventListener("DOMContentLoaded", function() {
-  /**
-   * HomePage - Help section
-   */
-  class Help {
-    constructor($el) {
-      this.$el = $el;
-      this.$buttonsContainer = $el.querySelector(".help--buttons");
-      this.$slidesContainers = $el.querySelectorAll(".help--slides");
-      this.currentSlide = this.$buttonsContainer.querySelector(".active").parentElement.dataset.id;
-      this.init();
-    }
-
-    init() {
-      this.events();
-    }
-
-    events() {
-      /**
-       * Slide buttons
-       */
-      this.$buttonsContainer.addEventListener("click", e => {
-        if (e.target.classList.contains("btn")) {
-          this.changeSlide(e);
-        }
-      });
-
-      /**
-       * Pagination buttons
-       */
-      this.$el.addEventListener("click", e => {
-        if (e.target.classList.contains("btn") && e.target.parentElement.parentElement.classList.contains("help--slides-pagination")) {
-          this.changePage(e);
-        }
-      });
-    }
-
-    changeSlide(e) {
-      e.preventDefault();
-      const $btn = e.target;
-
-      // Buttons Active class change
-      [...this.$buttonsContainer.children].forEach(btn => btn.firstElementChild.classList.remove("active"));
-      $btn.classList.add("active");
-
-      // Current slide
-      this.currentSlide = $btn.parentElement.dataset.id;
-
-      // Slides active class change
-      this.$slidesContainers.forEach(el => {
-        el.classList.remove("active");
-
-        if (el.dataset.id === this.currentSlide) {
-          el.classList.add("active");
-        }
-      });
-    }
+document.addEventListener("DOMContentLoaded", function () {
 
     /**
-     * TODO: callback to page change event
+
+     * HomePage - Help section
+
      */
-    changePage(e) {
-      e.preventDefault();
-      const page = e.target.dataset.page;
+
+    class Help {
+        constructor($el) {
+            this.$el = $el;
+            this.$buttonsContainer = $el.querySelector(".help--buttons");
+            this.$slidesContainers = $el.querySelectorAll(".help--slides");
+            this.currentSlide = this.$buttonsContainer.querySelector(".active").parentElement.dataset.id;
+
+            this.init();
+
+        }
+
+       init() {
+
+            this.events();
+        }
+
+        events() {
+            /**
+             * Slide buttons
+             */
+            this.$buttonsContainer.addEventListener("click", e => {
+                if (e.target.classList.contains("btn")) {
+                    this.changeSlide(e);
+                }
+            });
+
+           /**
+           * Pagination buttons
+           */
+            this.$el.addEventListener("click", e => {
+                if (e.target.classList.contains("btn") && e.target.parentElement.parentElement.classList.contains("help--slides-pagination")) {
+                    this.changePage(e);
+                }
+            });
+        }
+
+        changeSlide(e) {
+            e.preventDefault();
+            const $btn = e.target;
+
+            // Buttons Active class change
+            [...this.$buttonsContainer.children].forEach(btn => btn.firstElementChild.classList.remove("active"));
+
+            $btn.classList.add("active");
+
+            // Current slide
+            this.currentSlide = $btn.parentElement.dataset.id;
+
+            // Slides active class change
+
+            this.$slidesContainers.forEach(el => {
+                el.classList.remove("active");
+
+        if (el.dataset.id === this.currentSlide) {
+            el.classList.add("active");
+        }
+            });
+        }
+
+
+        /**
+
+         * TODO: callback to page change event
+
+         */
+
+        changePage(e) {
+
+            e.preventDefault();
+            const page = e.target.dataset.page;
 
       console.log(page);
+        }
     }
-  }
-  const helpSection = document.querySelector(".help");
-  if (helpSection !== null) {
-    new Help(helpSection);
-  }
+    const helpSection = document.querySelector(".help");
+    if (helpSection !== null) {
+        new Help(helpSection);
+    }
 
   /**
    * Form Select
    */
+
   class FormSelect {
-    constructor($el) {
-      this.$el = $el;
-      this.options = [...$el.children];
-      this.init();
-    }
+      constructor($el) {
+          this.$el = $el;
+          this.options = [...$el.children];
 
-    init() {
-      this.createElements();
-      this.addEvents();
-      this.$el.parentElement.removeChild(this.$el);
-    }
+          this.init();
+      }
 
-    createElements() {
-      // Input for value
-      this.valueInput = document.createElement("input");
-      this.valueInput.type = "text";
-      this.valueInput.name = this.$el.name;
+      init() {
+          this.createElements();
+          this.addEvents();
+          this.$el.parentElement.removeChild(this.$el);
+      }
 
-      // Dropdown container
-      this.dropdown = document.createElement("div");
-      this.dropdown.classList.add("dropdown");
 
-      // List container
-      this.ul = document.createElement("ul");
+      createElements() {
+          // Input for value
+          this.valueInput = document.createElement("input");
+          this.valueInput.type = "text";
+          this.valueInput.name = this.$el.name;
 
-      // All list options
-      this.options.forEach((el, i) => {
-        const li = document.createElement("li");
-        li.dataset.value = el.value;
-        li.innerText = el.innerText;
+
+          // Dropdown container
+          this.dropdown = document.createElement("div");
+          this.dropdown.classList.add("dropdown");
+
+          // List container
+          this.ul = document.createElement("ul");
+
+          // All list options
+          this.options.forEach((el, i) => {
+              const li = document.createElement("li");
+              li.dataset.value = el.value;
+              li.innerText = el.innerText;
+
 
         if (i === 0) {
-          // First clickable option
-          this.current = document.createElement("div");
-          this.current.innerText = el.innerText;
-          this.dropdown.appendChild(this.current);
-          this.valueInput.value = el.value;
-          li.classList.add("selected");
+            // First clickable option
+            this.current = document.createElement("div");
+            this.current.innerText = el.innerText;
+            this.dropdown.appendChild(this.current);
+            this.valueInput.value = el.value;
+            li.classList.add("selected");
         }
+
 
         this.ul.appendChild(li);
-      });
+          });
+
 
       this.dropdown.appendChild(this.ul);
-      this.dropdown.appendChild(this.valueInput);
-      this.$el.parentElement.appendChild(this.dropdown);
-    }
+          this.dropdown.appendChild(this.valueInput);
+          this.$el.parentElement.appendChild(this.dropdown);
+      }
 
-    addEvents() {
-      this.dropdown.addEventListener("click", e => {
-        const target = e.target;
-        this.dropdown.classList.toggle("selecting");
 
-        // Save new value only when clicked on li
-        if (target.tagName === "LI") {
-          this.valueInput.value = target.dataset.value;
-          this.current.innerText = target.innerText;
-        }
-      });
-    }
+      addEvents() {
+          this.dropdown.addEventListener("click", e => {
+              const target = e.target;
+              this.dropdown.classList.toggle("selecting");
+
+              // Save new value only when clicked on li
+              if (target.tagName === "LI") {
+                  this.valueInput.value = target.dataset.value;
+                  this.current.innerText = target.innerText;
+              }
+          });
+      }
   }
-  document.querySelectorAll(".form-group--dropdown select").forEach(el => {
-    new FormSelect(el);
-  });
 
-  /**
-   * Hide elements when clicked on document
-   */
-  document.addEventListener("click", function(e) {
-    const target = e.target;
-    const tagName = target.tagName;
+    document.querySelectorAll(".form-group--dropdown select").forEach(el => {
+        new FormSelect(el);
+    });
+
+
+    /**
+     * Hide elements when clicked on document
+     */
+
+    document.addEventListener("click", function (e) {
+        const target = e.target;
+        const tagName = target.tagName;
 
     if (target.classList.contains("dropdown")) return false;
 
     if (tagName === "LI" && target.parentElement.parentElement.classList.contains("dropdown")) {
-      return false;
+        return false;
     }
 
     if (tagName === "DIV" && target.parentElement.classList.contains("dropdown")) {
-      return false;
+        return false;
     }
 
+
     document.querySelectorAll(".form-group--dropdown .dropdown").forEach(el => {
-      el.classList.remove("selecting");
+        el.classList.remove("selecting");
     });
-  });
+    });
+
 
   /**
    * Switching between form steps
    */
+
   class FormSteps {
-    constructor(form) {
-      this.$form = form;
-      this.$next = form.querySelectorAll(".next-step");
-      this.$prev = form.querySelectorAll(".prev-step");
-      this.$step = form.querySelector(".form--steps-counter span");
-      this.currentStep = 1;
+      constructor(form) {
+          this.$form = form;
+          this.$next = form.querySelectorAll(".next-step");
+          this.$prev = form.querySelectorAll(".prev-step");
+          this.$step = form.querySelector(".form--steps-counter span");
+          this.currentStep = 1;
+
 
       this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
-      const $stepForms = form.querySelectorAll("form > div");
-      this.slides = [...this.$stepInstructions, ...$stepForms];
+          const $stepForms = form.querySelectorAll("form > div");
+          this.slides = [...this.$stepInstructions, ...$stepForms];
 
       this.init();
-    }
+      }
 
-    /**
-     * Init all methods
-     */
-    init() {
-      this.events();
-      this.updateForm();
-    }
 
-    /**
-     * All events that are happening in form
-     */
-    events() {
-      // Next step
-      this.$next.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep++;
+      /**
+       * Init all methods
+       */
+
+      init() {
+          this.events();
           this.updateForm();
-        });
-      });
+      }
 
-      // Previous step
-      this.$prev.forEach(btn => {
-        btn.addEventListener("click", e => {
-          e.preventDefault();
-          this.currentStep--;
-          this.updateForm();
-        });
-      });
 
-      // Form submit
-      this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
-    }
+      /**
+       * All events that are happening in form
+       */
 
-    /**
-     * Update form front-end
-     * Show next or previous section etc.
-     */
-    updateForm() {
-      this.$step.innerText = this.currentStep;
+      events() {
+          // Next step
+          this.$next.forEach(btn => {
+              btn.addEventListener("click", e => {
 
-      // TODO: Validation
+                  e.preventDefault();
+                  this.currentStep++;
+                  this.updateForm();
+              });
+          });
+
+
+          // Previous step
+          this.$prev.forEach(btn => {
+              btn.addEventListener("click", e => {
+                  e.preventDefault();
+                  this.currentStep--;
+                  this.updateForm();
+              });
+          });
+
+
+          // Form submit
+          this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
+      }
+
+      /**
+       * Update form front-end
+       * Show next or previous section etc.
+       */
+
+      updateForm() {
+          this.$step.innerText = this.currentStep;
+
+          // TODO: Validation
 
       this.slides.forEach(slide => {
-        slide.classList.remove("active");
+          slide.classList.remove("active");
 
-        if (slide.dataset.step === this.currentStep) {
-          slide.classList.add("active");
+        if (slide.dataset.step == this.currentStep) {
+            slide.classList.add("active");
         }
       });
 
       this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 6;
-      this.$step.parentElement.hidden = this.currentStep >= 6;
+          this.$step.parentElement.hidden = this.currentStep >= 6;
 
-      // TODO: get data from inputs and show them in summary
+          // TODO: get data from inputs and show them in summary
 
-      if (this.currentStep == 5) {
-        const categories = document.getElementById('categories').value
-        // let institution = document.querySelectorAll('input[name="organization"]');
-        // for (const i of institution) {
-        //   if (i.checked) {
-        //     institution = i.defaultValue;
-        //     break;
-        //   }
-        const institution = document.getElementById('instytucja').value
-        const number = document.getElementById('number').value
-        const address = document.getElementById('address').value
-        const city = document.getElementById('city').value
-        const postcode = document.getElementById('postcode').value
-        const phone = document.getElementById('phone').value
-        const date = document.getElementById('date').value
-        const time = document.getElementById('time').value
-        const more_info = document.getElementById('more_info').value
-        console.log(address)
-        console.log(city)
-        console.log(postcode)
-        console.log(phone)
-        console.log(date)
-        console.log(time)
-        console.log(more_info)
-        console.log(institution)
-        console.log(number)
-        console.log(categories)
 
-        const div1 = document.getElementById('droga')
-        const div2 = document.getElementById('miasto')
-        const div3 = document.getElementById('kod')
-        const div4 = document.getElementById('telefon')
-        const div5 = document.getElementById('data')
-        const div6 = document.getElementById('czas')
-        const div7 = document.getElementById('uwagi')
-        const div8 = document.getElementById('worki')
-        const div9 = document.getElementById('instytucja')
+      let bags = document.getElementById("bags");
+      // let cat = document.getElementById("categories");
+          document.querySelector("#bags2").innerText = bags.value
+          // document.querySelector("#bags2").innerText = bags.value; //+ ' worki z: ' + cat.value;
 
-          div1.innerText = address
-          div2.innerText = city
-          div3.innerText = postcode
-          div4.innerText = phone
-          div5.innerText = date.toString()
-          div6.innerText = time
-          div7.innerText = more_info
-          div8.innerText = number
-          div9.innerText = institution
-      }}
+      let organization = document.querySelector('input[name="organization"]');
+          document.querySelector("#organization2").innerText = organization.value;
 
-    /**
-     * Submit form
-     *
-     * TODO: validation, send data to server
-     */
-    submit(e) {
-      e.preventDefault();
-      this.currentStep++;
-      this.updateForm();
+      let address = document.getElementById("address");
+          document.querySelector("#address2").innerText = address.value;
+
+      let city = document.getElementById("city");
+          document.querySelector("#city2").innerText = city.value;
+
+      let postcode = document.getElementById("postcode");
+          document.querySelector("#postcode2").innerText = postcode.value;
+
+      let phone = document.getElementById("phone");
+          document.querySelector("#phone2").innerText = phone.value;
+
+      let date = document.getElementById("date");
+          document.querySelector("#date2").innerText = date.value;
+
+      let time = document.getElementById("time");
+          document.querySelector("#time2").innerText = time.value;
+
+      let comment = document.getElementById("info");
+          document.querySelector("#info2").innerText = comment.value;
+
+      }
+
+      /**
+       * Submit form
+       *
+       * TODO: validation, send data to server
+       */
+
+      submit(e) {
+          e.preventDefault();
+          this.currentStep++;
+          this.updateForm();
+
+          // fetch(zbiera info i wysyła do backend pod adres gdzie POST) class FormData w JS
+
+
+          fetch('/add_donation/', {
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-type': 'application/json',
+              },
+              body: JSON.stringify(data),
+          })
+              .then(response => response.json())
+              .then(data => {
+                  console.log('Success:', data);
+              })
+
+              .catch((error) => {
+                  console.error('Error:', error);
+              });
+      };
+  }
+    const form = document.querySelector(".form--steps");
+    if (form !== null) {
+        new FormSteps(form);
     }
-  }
-  const form = document.querySelector(".form--steps");
-  if (form !== null) {
-    new FormSteps(form);
-  }
 });
-
-
-// fetch('/add_donation/', {
-//         method: 'POST',
-//         headers: {
-//       'Accept': 'application/json',
-//       'Content-type': 'application/json',
-//     },
-//         body: JSON.stringify(data),
-//       })
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log('Success:', data);
-//       })
-//       .catch((error) => {
-//         console.error('Error:', error);
-//       });
